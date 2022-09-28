1da6b0ae8b59b2dba306accb8e87591ab159ba04
Lucas Santana <lucas_377@hotmail.com>
2022-09-28 20:37:06 -0300

    SQL Injection prevention

--- insert-student.php
+++ insert-student.php
@@ -7,8 +7,17 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/database.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$student = new Student(null, 'Lucas Santana', new DateTimeImmutable('1992-11-01'));
+$student = new Student(
+  null,
+  'Lucas Santana',
+  new DateTimeImmutable('1992-11-01')
+);
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
 
-$pdo->exec($sqlInsert);
+if ($statement->execute()) {
+  echo "Aluno inserido";
+}
